5675af526acad7aa4c05c5f727b9c90a18b1a9ac
Alidabi10 <112751131+Alidabi10@users.noreply.github.com>
2025-11-05 20:55:52 +0100

    transportuj_flote
    zejsciowa3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,28 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int sumaTowaru = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    Stocznia stocznia; //wywolujemy konstruktor domyslny stoczni
+
+    while (sumaTowaru < towar)
+    {
+        //zamawiamy statki (losowe)    
+        Statek* nowyStatek = stocznia(); //wywolanie metody specjalnej
+		//(przeciazony operator()), ktory zwraca wskaznik do nowego statku
+        //Transportujemy towar i zliczamy go do sumyTowaru
+        sumaTowaru += nowyStatek->transportuj();
+
+        if (dynamic_cast<Zaglowiec*>(nowyStatek))
+        {
+            liczbaZaglowcow++;
+        };
+        //zwalniamy pamiec - utylizujemy statek, ktory wykonal swoja robotwe
+        delete nowyStatek;
+    };
+
+    
+
+    return liczbaZaglowcow;
 }
